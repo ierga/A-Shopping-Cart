ed679c9fd355e0d8b243b29a5fe68afbedd9b38f
Ilkhom <iergashbaev@gmail.com>
2018-05-06 19:02:32 +0200

    propTypes for CartItem has been defined

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import './CartItem.css';
 
+const cartItemShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+})
+
+
 export default class CartItem extends Component {
   render() {
     const { name, price } = this.props
